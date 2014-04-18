b60989466578027836063003126a449f9dfc9de3
Francisco Soto <bobby@hellocode.mx>
2014-04-17 20:33:05 -0700

    Correctly configure hostname without needing a reboot.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -23,13 +23,11 @@ EOF
     rm /home/vagrant/temp-script.sh
 }
 
-# Install Ruby 2.1.0
-print_message 'Installing Ruby 2.1.0...'
-execute_with_rbenv "rbenv install 2.1.0 ; rbenv global 2.1.0"
-
-# Install Rails 4.1.0
-print_message 'Installing Rails 4.1.0...'
-execute_with_rbenv "gem install rails --version 4.1.0"
+# Setup hostname
+print_message "Setup hostname..."
+echo "hellocode" > /etc/hostname
+echo "127.0.0.1 hellocode" >> /etc/hosts
+hostname hellocode
 
 # Setup environment
 print_message "Setup environment..."
@@ -41,6 +39,7 @@ export PS1="[\[\033[1;34m\]\u\[\033[0m\]@\h:\[\033[1;37m\]\w\[\033[0m\]]$ "
 alias restart_server='rake db:drop; rake db:create; rake db:migrate; rails server'
 alias recreate_db='rake db:drop; rake db:create: rake db:migrate'
 alias get-repositories='bash /vagrant/scripts/get-repositories.sh'
+alias goto-store='cd /vagrant/store'
 
 # Load secret keys, if any.
 if [ -f ~/.secret_keys.sh ]; then
@@ -57,10 +56,13 @@ touch /home/vagrant/.secret_keys.sh
 chown vagrant:vagrant /home/vagrant/.environment.sh
 chown vagrant:vagrant /home/vagrant/.secret_keys.sh
 
-# Setup hostname
-print_message "Setup hostname..."
-echo "hellocode" > /etc/hostname
-echo "127.0.0.1 hellocode" >> /etc/hosts
+# Install Ruby 2.1.0
+print_message 'Installing Ruby 2.1.0...'
+execute_with_rbenv "rbenv install 2.1.0 ; rbenv global 2.1.0"
+
+# Install Rails 4.1.0
+print_message 'Installing Rails 4.1.0...'
+execute_with_rbenv "gem install rails --version 4.1.0"
 
 print_message "Do NOT forget to get the source code using the 'get-repositories' command."
 
