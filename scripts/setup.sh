355f0c922d5ff47d7f8834876818f4e5f4d423a7
Francisco Soto <bobby@hellocode.mx>
2014-04-17 19:02:05 -0700

    Setup a nicer environment for the application.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -31,13 +31,38 @@ execute_with_rbenv "rbenv install 2.1.0 ; rbenv global 2.1.0"
 print_message 'Installing Rails 4.1.0...'
 execute_with_rbenv "gem install rails --version 4.1.0"
 
-# Setup a blue prompt.
-print_message "Setup prompt..."
-echo 'export PS1="[\[\033[1;34m\]\u\[\033[0m\]@\h:\[\033[1;37m\]\w\[\033[0m\]]$ "' >> /home/vagrant/.bash_profile
+# Setup environment
+print_message "Setup environment..."
+`cat >/home/vagrant/.environment.sh <<\EOF
+# Environment variables
+export PS1="[\[\033[1;34m\]\u\[\033[0m\]@\h:\[\033[1;37m\]\w\[\033[0m\]]$ "
+
+# Aliases to make our life easier.
+alias restart_server='rake db:drop; rake db:create; rake db:migrate; rails server'
+alias recreate_db='rake db:drop; rake db:create: rake db:migrate'
+alias clone-project-repos='bash /vagrant/scripts/get-repositories.sh'
+
+# Load secret keys, if any.
+if [ -f ~/.secret_keys.sh ]; then
+  source ~/.secret_keys.sh
+fi
+
+EOF
+`
+
+echo 'source ~/.environment.sh' >> /home/vagrant/.bash_profile
+
+touch /home/vagrant/.secret_keys.sh
+
+chown vagrant:vagrant /home/vagrant/.environment.sh
+chown vagrant:vagrant /home/vagrant/.secret_keys.sh
 
 # Setup hostname
 print_message "Setup hostname..."
 echo "hellocode" > /etc/hostname
 echo "127.0.0.1 hellocode" >> /etc/hosts
+
+print_message "Cloning the project repositories..."
+su vagrant -c "bash /vagrant/scripts/get-repositories.sh"
 
 print_message 'Done.'
